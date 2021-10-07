961b16c8d2da5bbb309f21693d4431516f274dc6
MMartina020 <marcsikaa15@gmail.com>
2021-10-07 13:44:01 +0200

    valami

--- index.js
+++ index.js
@@ -3,8 +3,39 @@ const port = 4444;
 const fs = require('fs');
 
 const server = http.createServer((req, res) =>{
-    res.write(`A kérés forrása ${req.url}`);
-    res.end("         -Vége");
+    console.log(req.url);
+    console.log(req.method);
+    
+   
+    //router 
+    switch (true){
+        case req.url === '/' && req.method === 'GET':
+            fs.readFile('./views/index.html', (err,file) =>{
+                res.setHeader('Content-Type', 'text/html');
+                res.end(file);
+            });
+            break;
+            
+
+            case req.url === '/' && req.method === 'GET':
+            fs.readFile('./views/minta2.css', (err,file) =>{
+                res.setHeader('Content-Type', 'text/css');
+                res.end(file);
+            });
+            break;
+            case req.url === '/telefon' && req.method === 'GET':
+                kiszolgal('./telefon', (err,file) =>{
+
+                });
+                break;
+            
+        default:
+            fs.readFile('./views/hiba404.html', (err,file) =>{
+            res.setHeader('Content-Type', 'text/html');
+            res.end(file);
+            });
+            //Majd ezt is küldöm állományból
+    }
 })
 
 server.listen(port);
